e214458d15eee0982953d3b75bf45f1ceb81580a
Katsuya Asakura <04asaka02@gmail.com>
2022-11-13 15:36:16 +0900

    15分通知のテキストを修正。

--- sprintNotification.sh
+++ sprintNotification.sh
@@ -1,4 +1,4 @@
 # 通知ダイアログを表示
 echo "***** 15 Minutes Timer Started!! *****"
-sleep $((60 * 15)); osascript -e 'display notification "15分経過しました！"'
+sleep $((60 * 15)); osascript -e 'display notification ""😘😘😘 15分経過しました！ 😘😘😘"" with title "My Timer"'
 echo "***** End 15 Minutes Timer!! *****"
